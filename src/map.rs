3f18d43c712ea9d47bff373d6f2497b313f2b59b
Christian Engen <chris.a.engen@gmail.com>
2022-06-30 21:20:55 +0200

    add simple map algo

--- src/map.rs
+++ src/map.rs
@@ -68,7 +68,7 @@ fn apply_vertical_tunnel(map: &mut [TileType], y1: i32, y2: i32, x: i32) {
     }
 }
 
-pub fn new_map_rooms_and_corridors() -> Vec<TileType> {
+pub fn new_map_rooms_and_corridors() -> (Vec<Rect>, Vec<TileType>) {
     let mut map = vec![TileType::Wall; (MAP_WIDTH * MAP_HEIGHT) as usize];
 
     let mut rooms: Vec<Rect> = Vec::new();
@@ -92,11 +92,23 @@ pub fn new_map_rooms_and_corridors() -> Vec<TileType> {
         }
         if ok {
             apply_room_to_map(&new_room, &mut map);
+            if !rooms.is_empty() {
+                let (new_x, new_y) = new_room.center();
+                let (prev_x, prev_y) = rooms[rooms.len() - 1].center();
+                if rng.range(0, 2) == 1 {
+                    apply_horizontal_tunnel(&mut map, prev_x, new_x, prev_y);
+                    apply_vertical_tunnel(&mut map, prev_y, new_y, new_x);
+                } else {
+                    apply_vertical_tunnel(&mut map, prev_y, new_y, prev_x);
+                    apply_horizontal_tunnel(&mut map, prev_x, new_x, new_y);
+                }
+            }
+
             rooms.push(new_room);
         }
     }
 
-    map
+    (rooms, map)
 }
 
 pub fn draw_map(map: &[TileType], ctx: &mut Rltk) {
